ef3ea91daf173d39c6309fc948f6911d3b15c4a7
MLMunoz85 <matthew.l.munoz@gmail.com>
2022-12-10 17:20:36 -0800

    Completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,8 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+
+
 const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
 const numbersArray2 = [24, 27, 30, 33, 36]
@@ -23,8 +25,28 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+// Pseudocode
+// create a function called divThree
+// input: an object
+// output: a string that says "number is divisible by three" if the number is evenly divisible by three and "number is not divisible by three" if the number is not evenly divisible by three.
+
+// Process: will create a function called divThree that has object as a parameter. The function will create a condition statement that if the number can be evenly divided by three, the output will say "number is divided by three". Else, the output will say "number is not divisible by three"
+
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("divThree", () => {
+    it("takes an object as an argument and decides if the number inside it is evenly divisible by three or not", () => {
+        const object1 = { number: 15 }
+        // Expected output: "15 is divisible by three"
+        const object2 = { number: 0 }
+        // Expected output: "0 is divisible by three"
+        const object3 = { number: -7 }
+        // Expected output: "-7 is not divisible by three"
+        expect(divThree(object1)).toEqual("15 is divisible by three")
+        expect(divThree(object2)).toEqual("0 is divisible by three")
+        expect(divThree(object3)).toEqual("-7 is not divisible by three")
+    })
+})
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
 const object2 = { number: 0 }
@@ -34,9 +56,35 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
+const divThree = (object) => {
+    if (object.number % 3 === 0) {
+        return `${object.number} is divisible by three`
+    } else {
+        return `${object.number} is not divisible by three`
+    }
+}
+
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
+// Pseudocode
+// create a function called allCap 
+// input: an array
+// output: takes in an array of words and returns an array with all the first letter in all the words capitalized
+
+// Process: I will create a function called allCap that will take in an array as a parameter. Using the higher order function map and charAt to capitalize the first letter of each string in the array, then using slice to make the rest of the string from index 1 on back into lowercase.
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("allCap", () => {
+    it("takes in an array of words and returns an array with all the first letter in all the words capitalized", () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+        expect(allCap(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(allCap(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+    })
+})
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -45,9 +93,35 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+const allCap = (array) => {
+    return array.map(value => {
+        return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    })
+}
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
+// Pseudocode
+// create a function called firstVowel
+// input: string
+// output: logs the index of the first vowel
+
+// Process: I will create a function named firstVowel. I will make a for loop that checks each index in the string and outputs the index where the first vowel is. In order to do this I will use the string match() built-in method to search for an array of vowels and charAt() to find the index of the vowel.
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("firstVowel", () => {
+    it("takes in a string and logs the index of the first vowel", () => {
+        const vowelTester1 = "learn"
+        // Expected output: 1
+        const vowelTester2 = "academy"
+        // Expected output: 0
+        const vowelTester3 = "challenges"
+        // Expected output: 2
+        expect(firstVowel(vowelTester1)).toEqual(1) 
+        expect(firstVowel(vowelTester2)).toEqual(0) 
+        expect(firstVowel(vowelTester3)).toEqual(2) 
+    })
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -57,3 +131,11 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+
+const firstVowel = (string) => {
+    for (let i=0; i < string.length; i++) {
+        if (string.charAt(i).match(/[aeiou]/)) {
+            return i
+        }
+    }
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 const cohort = "Hotel 2022"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:["H", "o", "t", "e", "l", " ", "2", "0", "0", "2"]
+// b) Verify and explain: ["H", "o", "t", "e", "l", " ", "2", "0", "0", "2"], this is because split converts a string into an array. The ("") means that it will split the string by each character or value.
 
 // --------------------2) What will this log?
 
@@ -20,24 +20,24 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "LEARN Student"
+// b) Verify and explain: undefined, in order for "LEARN Student" to be displayed using string interpolation the string will need to be set to a variable. Then when you console log you would console.log(greeter(variableName)) for it to work.
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
 // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: [8, 10, 12, 14, 16], this is because map will do something to each item in the array, in this case multiply each item by 2. It will then return an array with the new items of the same length.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: [11, 13, 15], this is because the higher order function filter is filtering items from the array. In this case it's checking for numbers that cannot be divided by 2 evenly.
 
 // --------------------5) What will this log?
 
@@ -49,8 +49,8 @@ const myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: JavaScript, this is because  the console log is looking in the myCodingSkills object for the 0 index or first value of the languages array. In this case that would be JavaScript.
 
 // --------------------STRETCH: What will this log?
 
@@ -62,7 +62,8 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: 'George', cohort: 'Hotel', year: 2022 }
+// b) Verify and explain: Learn { student: 'George', cohort: 'Hotel', year: 2022 }
+// This is because it is making a new object learnStudent using the Learn class for George. Hotel and 2022 are also displaying because those are default values for the object.
